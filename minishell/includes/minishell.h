49b75ff6045c1dca9cc89ebfad5522d06e184b2a
Esilbor <boris.b.bresil@gmail.com>
2024-01-10 12:46:48 +0100

    working version with updated heredoc mod from yesterday

--- minishell/includes/minishell.h
+++ minishell/includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: bbresil <bbresil@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 12:05:45 by bbresil           #+#    #+#             */
-/*   Updated: 2024/01/09 17:45:45 by bbresil          ###   ########.fr       */
+/*   Updated: 2024/01/10 12:43:47 by bbresil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -182,7 +182,8 @@ void		ft_dup2(t_set *set, int index);
 /* heredocs.c */
 
 int			fill_heredoc(char *buf, t_env *env, int fd, char *limiter);
-void		create_heredoc(t_env *env, t_lexer *lex, char *limiter);
+// void		create_heredoc(t_env *env, t_lexer *lex, char *limiter);
+int			create_heredoc(t_env *env, t_lexer *lex, char *limiter);
 int			init_heredocs(t_env *env, t_cmd **cmd_tab);
 
 /* heredocs.c */
